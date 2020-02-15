c6b023bced28ebd2d7fa1e0f283ab7684e2da78e
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2020-02-15 13:38:11 +1100

    controlle test

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -2,12 +2,28 @@ package controller_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+    . "github.com/onsi/gomega"
+    
+    context "context"
 
-	. "github.com/bythepowerof/gqlgen-kmakeapi/controller"
+    . "github.com/bythepowerof/gqlgen-kmakeapi/controller"
+    k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+    "github.com/bythepowerof/gqlgen-kmakeapi/k8s"
+
 )
 
 var _ = Describe("Controller", func() {
+    var k k8sclient.Client
+    var kmc *KubernetesController
+
+	BeforeEach(func() {
+		var err error
+		k, err = k8s.FakeK8sClient()
+        Expect(err).To(BeNil())
+        
+        kmc = &KubernetesController{ Client: k}
+	})
+
     Describe("KubernetesController", func() {
         Context("implements interface", func() {
             It("KmakeController", func() {
@@ -19,6 +35,14 @@ var _ = Describe("Controller", func() {
                 var p interface{} = &v
                 _, ok = p.(KmakeController)
                 Expect(ok).To(Equal(true))
+            })
+        })
+        Context("fetches", func() {
+            It("kmake", func() {
+                ns := "ns1"
+                kmakes, err := kmc.Kmakes(context.Background(), &ns, nil)
+                Expect(err).To(BeNil())
+                Expect(kmakes[0].GetName()).To(Equal("test-kmake"))
             })
         })
     })
