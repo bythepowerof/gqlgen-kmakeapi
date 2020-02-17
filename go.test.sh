aa203658cfede45f5994ac7d8d5573dbe7a4b40c
Jeremy Marshall <jeremystuartmarshall@gmail.com>
2020-02-18 08:30:46 +1100

    fix test script

--- go.test.sh
+++ go.test.sh
@@ -4,8 +4,10 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic -coverpkg github.com/bythepowerof/gqlgen-kmakeapi/controller,github.com/bythepowerof/gqlgen-kmakeapi/k8s,github.com/bythepowerof/gqlgen-kmakeapi/view
- "$d"
+    go test -race -coverprofile=profile.out \
+        -covermode=atomic \
+        -coverpkg github.com/bythepowerof/gqlgen-kmakeapi/controller,github.com/bythepowerof/gqlgen-kmakeapi/k8s,github.com/bythepowerof/gqlgen-kmakeapi/view \
+        "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
